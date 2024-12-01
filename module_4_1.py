8771549ed925b65f2ee79657444922f31e1e7370
hotabychiot <hotabychiot@gmail.com>
2024-12-01 11:20:31 +0300

    Модули и пакеты

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as divide_fake
+from true_math import divide as divide_true
+
+print(divide_fake(69, 3))
+print(divide_fake(3, 0))
+print(divide_true(49, 7))
+print(divide_true(15, 0))
+
